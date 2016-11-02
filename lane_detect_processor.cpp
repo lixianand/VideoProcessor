fe21fc1f798d2f4a291135c9b61b6d66e04013f8
Nathan Greco <nathan.greco@gmail.com>
2016-11-02 11:06:34 -0400

    Added more filtering for L/R contour angle

--- lane_detect_processor.cpp
+++ lane_detect_processor.cpp
@@ -291,10 +291,12 @@ void SortContours( const std::vector<EvaluatedContour>& evaluatedsegments,
 		if ( evaluatedcontour.ellipse.center.x < (imagewidth * 0.5f) ) {
 			//Filter by angle
 			if ( evaluatedcontour.angle < lanedetectconstants::kminimumangle ) return;
+			if ( evaluatedcontour.angle > 100.0f ) return;
 			leftcontours.push_back( evaluatedcontour );
 		} else {
 			//Filter by angle
 			if ( evaluatedcontour.angle > (180.0f - lanedetectconstants::kminimumangle) ) return;
+			if ( evaluatedcontour.angle < 80.0f ) return;
 			rightcontours.push_back( evaluatedcontour );
 		}
 	}
